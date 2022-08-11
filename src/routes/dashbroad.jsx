c489204ab531932dfed1c478011662a0f9fbe2d8
Hướng Võ <huongvovan123@gmail.com>
2022-08-11 09:20:49 +0700

    fix dash

--- src/routes/dashbroad.jsx
+++ src/routes/dashbroad.jsx
@@ -2,10 +2,11 @@ import React, { useEffect } from 'react'
 import { Container } from '../styles/containers';
 import Dashbroad from '../components/Dashbroad';
 import { useAPIContext } from '../contexts/APIContext';
+import { useNavigate } from 'react-router-dom';
 
 const DashbroadRoute = () => {
   const { user, loadingUser } = useAPIContext();
-
+  const navigate = useNavigate();
   useEffect(() => {
     if (!loadingUser && !user) return navigate('/');
   }, [loadingUser, user]);
